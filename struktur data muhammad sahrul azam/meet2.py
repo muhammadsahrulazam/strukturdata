2a66067874cdfa44a09fcb664b629001639e1559
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-04-01 11:08:23 +0700

    Add files via upload
    1 april 2024

---
+++ struktur data muhammad sahrul azam/meet2.py
@@ -0,0 +1,20 @@
+# belajar variabel
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print("sisa uang saya =",uang)
+
+#penamaan variabel
+nilai_a = 9 # tidak boleh memakai spasi
+kotak10 = 10 # tidak bo;eh di awali angka
+
+# input variabel kedua
+x = 30
+print ("Nilai UAS saya =", x)
+x = 300
+print ("Nilai UAS saya =", x)
