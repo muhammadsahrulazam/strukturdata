01f45029d784bf152602790443526a51e68d7675
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-04-22 11:51:35 +0800

    exe.py 22-04-2024

---
+++ exe3.py
@@ -0,0 +1,21 @@
+# contoh tipe data (integer) : angka satuan
+bil_integer = 1
+print(type(bil_integer))
+print("Tipe data dari bilangan ini adalah:", type(bil_integer))
+
+# contoh tipe data (float) : bilangan desimal
+bil_float = 2,5
+print ("type data bil_float adalah: ", type (bil_float))
+
+# Contoh tipe data (string): kumpulan karakter
+bil_string = "muhammad sahrul azam."
+print("Tipe data dari string_data adalah:", type(bil_string))
+
+# Contoh tipe data (boolean): biner 0,1, True, False
+bil_bol = True
+print("Tipe data dari boolean_data adalah:", type(bil_bol))
+
+# tipe data dari bahasa c
+from ctypes import c_double 
+bil_double = c_double (2000000)
+print("Tipe data bil_double adalah:", type(bil_double))
