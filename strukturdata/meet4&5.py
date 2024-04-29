76d04b7ca42758759c5a830c30d616e52158e7f5
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-04-29 11:51:41 +0800

    senin 29-04-2024

---
+++ strukturdata/meet4&5.py
@@ -0,0 +1,48 @@
+# input dari user
+# arit matika
+
+# belajar inputan
+#sahrul = input ("masukan kata : ")
+#print ("isi dari sahrul : ", sahrul, "bertipe data :", type (sahrul))
+
+# belajar aritmatika dasar
+x = 11
+y = 4
+
+# penjumlahan +
+hasil = x + y
+print ("x + y =",hasil)
+
+# Pengurangan -
+hasil = x - y
+print("x - y =", hasil)
+
+# Perkalian *
+hasil = x * y
+print("x * y =", hasil)
+
+# Pembagian /
+hasil = x / y
+print("x / y =", hasil)
+
+# Perpangkatan **
+hasil = x ** y
+print("x ^ y =", hasil)
+
+# Modulo %
+hasil = y % x
+print("x % y =", hasil)
+
+# Floor Division //
+hasil = x // y
+print("x // y =", hasil)
+
+# aritmatika prioritas
+#(), exponen, perkalian/pembagian, penjumlahan/pengurangan
+x = 3
+y = 4
+z = 5
+
+hasil = (x ** y * ( z + x ) / y - z % x // y)
+print( hasil )
+
