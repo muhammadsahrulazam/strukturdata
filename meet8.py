f6f89e0f31269da29a40f9f418d007c1e60b0316
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-05-20 11:30:29 +0700

    senin 20 05 2024

---
+++ meet8.py
@@ -0,0 +1,34 @@
+# perbandingan lanjutan
+
+# +++++3-------9+++++
+masukanuser = float(input("Masukkan bilangan (kurang dari 3 atau lebih dari 9): "))
+
+# ----- cekk kurang dari 3
+kurDar = (masukanuser < 3)
+print ("kurang dari 3 =", kurDar)
+
+# ------ cek lebih dari 9
+lebDar = (masukanuser > 9)
+print ("lebih dari 9 =", lebDar)
+
+betul = kurDar or lebDar
+print ("pengecekan final :",betul)
+
+# 4 14
+# +++++4-------14+++++
+masukanuser = float(input("Masukkan bilangan (kurang dari 4 atau lebih dari 14): "))
+
+# ----- cekk kurang dari 4
+kurDar = (masukanuser < 4)
+print ("kurang dari 4 =", kurDar)
+
+# ------ cek lebih dari 14
+lebDar = (masukanuser > 14)
+print ("lebih dari 14 =", lebDar)
+
+betul = kurDar or lebDar
+print ("pengecekan final :",betul)
+
+# ---------5+++++++9-------12+++++++30------- pr
+
+
