b3f8a8913ee9991bd21c152276a6cf85602e8c79
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-06-03 11:04:10 +0700

    03 06 2024 sahrul

---
+++ strukturdata meet9/meet9.py
@@ -0,0 +1,25 @@
+# percabangan jika maka
+
+name = input ("what is yoour name :")
+print (name)
+
+# if kondisi : aksi
+
+#if standarif 
+#if name == "shrl" : print ("player hoki")
+#print ("end of program")
+
+
+# if identitas 
+#if name =="shrl" :
+#    print ("player hoki")
+#    print ("hoki banget")
+#print ("end of program")
+
+# if else 
+if name =="rawwr" :
+    print ("player hoki")
+    print ("hoki banget")
+else:
+    print ("anda tidak hoki")
+print ("end of program")
