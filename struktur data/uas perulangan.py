7aee94b08818231dc165338bca4bdb598e3c74bb
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2024-07-01 10:57:50 +0800

    UAS

--- struktur data/uas perulangan.py
+++ struktur data/uas perulangan.py
@@ -1,14 +1,6 @@
-def hitung_faktorial():
-    while True:
-        try:
-            num = int(input("Masukkan angka untuk dihitung faktorialnya (angka negatif untuk berhenti): "))
-            if num < 0:
-                break
-            faktorial = 1
-            for i in range(1, num + 1):
-                faktorial *= i
-            print(f"Faktorial dari {num} adalah {faktorial}")
-        except ValueError:
-            print("Input tidak valid. Harap masukkan angka.")
-
-hitung_faktorial()
+# Contoh 1: Looping dengan while
+print("\nContoh 1: Looping dengan while")
+angka = 0
+while angka < 500:
+    print(angka)
+    angka += 1
